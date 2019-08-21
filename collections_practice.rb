eb23c09dd2c2120c00b646eeeeaa047e47d31cd9
Maverick Adolfo <merkonical@gmail.com>
2019-08-21 13:52:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,21 @@
+def sort_array_asc(array_asc)
+  array_asc.sort 
+end
 
+def sort_array_desc(array_desc)
+  array_desc.sort do |a,b|
+    b <=> a
+  end 
+end 
+
+def sort_array_char_count(animals)
+  animals.sort do |a, b| 
+    a.length <=> b.length
+  end 
+end 
+
+def swap_elements(the_switch)
+  the_switch.sort do |a,b|
+    a[1] <=> b[2]
+  end 
+end 
